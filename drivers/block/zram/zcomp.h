09b1f528ee31d90ddb37fbb90ad6a53ab8268e96
ShivamKumarJha <jha.shivam3@gmail.com>
2024-01-24 22:46:15 +0900

    Add drivers/block/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- drivers/block/zram/zcomp.h
+++ drivers/block/zram/zcomp.h
@@ -7,19 +7,37 @@
  * 2 of the License, or (at your option) any later version.
  */
 
+#if IS_ENABLED(CONFIG_ZRAM_ZSTD_ADVANCED)
+#include <linux/mm.h>
+#include <linux/zstd.h>
+#else
+#include <linux/crypto.h>
+#endif
+
 #ifndef _ZCOMP_H_
 #define _ZCOMP_H_
 
 struct zcomp_strm {
 	/* compression/decompression buffer */
 	void *buffer;
+#if IS_ENABLED(CONFIG_ZRAM_ZSTD_ADVANCED)
+	ZSTD_parameters params;
+	ZSTD_CCtx *cctx;
+	ZSTD_DCtx *dctx;
+	void *cctx_wksp;
+	void *dctx_wksp;
+#else
 	struct crypto_comp *tfm;
+#endif
 };
 
 /* dynamic per-device compression frontend */
 struct zcomp {
 	struct zcomp_strm * __percpu *stream;
 	const char *name;
+#if IS_ENABLED(CONFIG_ZRAM_ZSTD_ADVANCED)
+	unsigned int compression_level;
+#endif
 	struct hlist_node node;
 };
 
@@ -28,7 +46,7 @@ int zcomp_cpu_dead(unsigned int cpu, struct hlist_node *node);
 ssize_t zcomp_available_show(const char *comp, char *buf);
 bool zcomp_available_algorithm(const char *comp);
 
-struct zcomp *zcomp_create(const char *comp);
+struct zcomp *zcomp_create(const char *comp, unsigned int compression_level);
 void zcomp_destroy(struct zcomp *comp);
 
 struct zcomp_strm *zcomp_stream_get(struct zcomp *comp);

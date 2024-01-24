bb840738a54a912ab9f5771f0541665097dfe676
ShivamKumarJha <jha.shivam3@gmail.com>
2024-01-24 22:46:18 +0900

    Add drivers/misc/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -1853,14 +1853,14 @@ static int qseecom_scale_bus_bandwidth_timer(uint32_t mode)
 		request_mode = mode;
 	}
 
-	ret = __qseecom_set_msm_bus_request(request_mode);
-	if (ret) {
-		pr_err("set msm bus request failed (%d),request_mode (%d)\n",
-			ret, request_mode);
-		goto err_scale_timer;
-	}
-
 	if (qseecom.timer_running) {
+		ret = __qseecom_set_msm_bus_request(request_mode);
+		if (ret) {
+			pr_err("set msm bus request failed (%d),request_mode (%d)\n",
+				ret, request_mode);
+			goto err_scale_timer;
+		}
+
 		ret = __qseecom_decrease_clk_ref_count(CLK_QSEE);
 		if (ret) {
 			pr_err("Failed to decrease clk ref count.\n");
